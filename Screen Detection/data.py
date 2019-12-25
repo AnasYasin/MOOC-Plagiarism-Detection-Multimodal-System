8526862a7a74eb62ee590c8645e9fab75c3fcda3
Anas Yasin <34492965+AnasYasin@users.noreply.github.com>
2019-12-26 03:23:19 +0500

    Update data.py

--- Screen Detection/data.py
+++ Screen Detection/data.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np      
 import os
+import sys
 
-dir_data = "sc/"
-sum_data_images = np.zeros((7,3))
-for i in range(7):
+number_of_images = 3
+dir_data = sys.argv[1] + '/'
+sum_data_images = np.zeros((number_of_images,3))
+for i in range(number_of_images):
     img_data = cv2.imread(dir_data + str(i+1) +'.png')
     scale = (img_data - img_data.min()) / (img_data.max() - img_data.min())
     sum_dataCh1 = np.sum(scale[:,:,0])
